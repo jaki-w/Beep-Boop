c27d1fe2ac9d0bbac472d46ccabb05c87b03a1a8
Jaki Widby <jaki.widby@gmail.com>
2019-12-21 20:07:15 -0800

    create working loop with index of 0 that counts up to input number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   $("form#numberForm").submit(function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  var inputNumber = parseInt($(number).val());
-  alert(inputNumber);
+    var inputNumber = parseInt($(number).val());
+    alert(inputNumber);
+    var i = inputNumber;
+
+      for (var index = 0; index <= inputNumber; index ++) {
+          alert(index);
+      }
 
   });
 });
