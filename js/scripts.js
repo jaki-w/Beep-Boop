184c91dbd7c8f39a37b01e5456f5989ee59ec82e
Jaki Widby <jaki.widby@gmail.com>
2019-12-23 20:30:53 -0800

    simplify else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
-var ouputNumber = function(userInput) {
-  var reply = [];
+var ouputNumber = function(inputNumber) {
+  var result = [];
 
-  for (var index = 0; index <= inputNumber; index += 1) {
-    if (index.includes("1")) {
-      reply.push("Beep!");
-    } else if (index.includes("2")) {
-      reply.push("Boop!");
-    } else if (index.includes("3")) {
-      reply.push("I'm sorry, Dave. I'm afraid I can't do that.");
+  for (var i = 0; i <= parseInt(inputNumber); i += 1) {
+    var string = i.toString();
+
+    if (string.includes("1")) {
+      result.push("Beep!");
+    } else if (string.includes("2")) {
+      result.push("Boop!");
+    } else if (string.includes("3")) {
+      result.push("I'm sorry, Dave. I'm afraid I can't do that.");
     } else {
-      reply.push(' ' + index);
+      result.push(' ' + string);
     }
   }
-  return finalList;
+  return result;
 }
 //everything below this line is user interface logic
 $(document).ready(function() {
